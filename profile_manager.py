3310a3392779167f7778cf81d4bc9c234848ece3
Ambarisha B <b.ambarisha@gmail.com>
2014-01-26 01:40:27 +0530

    Fixed 'datetime' serialization issue

--- profile_manager.py
+++ profile_manager.py
@@ -1,29 +1,49 @@
+import Queue
 from datetime import datetime, timedelta
-from common import log
+from common import log, fatal
 from json import load, dump
+
+import message
+
+def datetime_encode(obj):
+    if isinstance(obj, datetime):
+        dtbase = datetime(1970, 1, 1)
+        return int((obj - dtbase).total_seconds())
+    raise TypeError
+
+def profile_decode(dictionary):
+    d = {}
+    for key in dictionary:
+        d[key] = []
+        for reading in dictionary[key]:
+            d[key].append((datetime.utcfromtimestamp(reading[0]), reading[1]))
+    return d
 
 class ProfileManager:
     def __init__(self, recent = timedelta(days = 7), filename = '.dms_profiles'):
-        self.queue = Queue.queue()
+        self.queue = Queue.Queue()
         self._recent = recent
         self._filename = filename
-        _load(self._filename)
+        ret, val = self._load(self._filename)
+        if ret: fatal("Could not load profiles file: " + self._filename)
     
     def _load(self, filename):
         try:
             f = open(filename, 'r')
-            self._record = load(f)
+            self._record = load(f, object_hook = profile_decode)
+            return (0, None)
         except IOError as ioe:
-            return (1, ioe.strerr + ": " + filename)
+            return (1, ioe.strerror + ": " + filename)
         except ValueError as ve:
             return (2, filename)
 
     def _save(self, filename):
         try:
             f = open(filename, 'w')
-            dump(self._record, f)
+            dump(self._record, f, default = datetime_encode)
         except IOError as ioe:
             return (1, ioe.strerr + ": " + filename)
+
     def _update(self, site, speed, filesize):
         self._record[site].append((datetime.now(), speed))     
 
